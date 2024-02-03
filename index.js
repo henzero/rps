fbea0a277fa9f28a27c83045a79b1b0ad63f5945
henzero <135627431+henzero@users.noreply.github.com>
2024-02-03 22:56:47 +0800

    create one round function

--- index.js
+++ index.js
@@ -12,4 +12,31 @@ const getPlayerChoice = () => {
 	return player.charAt(0).toUpperCase() + player.slice(1).toLowerCase();
 };
 
-console.log(getPlayerChoice());
+const playRound = (player, computer) => {
+	let point = 0;
+
+	if (player == choices[1] && computer == choices[0]) {
+		console.log(`${player} vs ${computer}\n${choices[1]} beats ${choices[0]}\nYou win!`);
+		point++;
+	} else if (player == choices[0] && computer == choices[2]) {
+		console.log(`${player} vs ${computer}\n${choices[0]} beats ${choices[2]}\nYou win!`);
+		point++;
+	} else if (player == choices[2] && computer == choices[1]) {
+		console.log(`${player} vs ${computer}\n${choices[2]} beats ${choices[1]}\nYou win!`);
+		point++;
+	} else if (player == choices[0] && computer == choices[1]) {
+		console.log(`${player} vs ${computer}\n${choices[1]} beats ${choices[0]}\nYou lose!`);
+	} else if (player == choices[1] && computer == choices[2]) {
+		console.log(`${player} vs ${computer}\n${choices[2]} beats ${choices[1]}\nYou lose!`);
+	} else if (player == choices[2] && computer == choices[0]) {
+		console.log(`${player} vs ${computer}\n${choices[0]} beats ${choices[2]}\nYou lose!`);
+	} else {
+		for (let i = 0; i < choices.length; i++) {
+			if (player == choices[i]) {
+				console.log(`${player} vs ${computer}\nIt's a tie!`);
+			}
+		}
+	}
+
+	return point;
+};
