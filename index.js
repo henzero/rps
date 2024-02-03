eb67c191605afb98d740680e972ef6f485e1dbe4
henzero <135627431+henzero@users.noreply.github.com>
2024-02-03 22:26:27 +0800

    create player prompt function

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ const getComputerChoice = () => {
 
 	return choices[random];
 };
+
+const getPlayerChoice = () => {
+	let player = prompt("Rock, Paper, or Scissors?");
+
+	return player.charAt(0).toUpperCase() + player.slice(1).toLowerCase();
+};
+
+console.log(getPlayerChoice());
