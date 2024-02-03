fbc12c64eb9df7348adb71aff61c32f73a0c9573
henzero <135627431+henzero@users.noreply.github.com>
2024-02-03 23:09:06 +0800

    create play game function

--- index.js
+++ index.js
@@ -16,27 +16,41 @@ function playRound(player, computer) {
 	let point = 0;
 
 	if (player == choices[1] && computer == choices[0]) {
-		console.log(`${player} vs ${computer}\n${choices[1]} beats ${choices[0]}\nYou win!`);
+		console.log(`${player} vs ${computer}\n${choices[1]} beats ${choices[0]}\n--You win!--`);
 		point++;
 	} else if (player == choices[0] && computer == choices[2]) {
-		console.log(`${player} vs ${computer}\n${choices[0]} beats ${choices[2]}\nYou win!`);
+		console.log(`${player} vs ${computer}\n${choices[0]} beats ${choices[2]}\n--You win!--`);
 		point++;
 	} else if (player == choices[2] && computer == choices[1]) {
-		console.log(`${player} vs ${computer}\n${choices[2]} beats ${choices[1]}\nYou win!`);
+		console.log(`${player} vs ${computer}\n${choices[2]} beats ${choices[1]}\n--You win!--`);
 		point++;
 	} else if (player == choices[0] && computer == choices[1]) {
-		console.log(`${player} vs ${computer}\n${choices[1]} beats ${choices[0]}\nYou lose!`);
+		console.log(`${player} vs ${computer}\n${choices[1]} beats ${choices[0]}\n--You lose!--`);
 	} else if (player == choices[1] && computer == choices[2]) {
-		console.log(`${player} vs ${computer}\n${choices[2]} beats ${choices[1]}\nYou lose!`);
+		console.log(`${player} vs ${computer}\n${choices[2]} beats ${choices[1]}\n--You lose!--`);
 	} else if (player == choices[2] && computer == choices[0]) {
-		console.log(`${player} vs ${computer}\n${choices[0]} beats ${choices[2]}\nYou lose!`);
+		console.log(`${player} vs ${computer}\n${choices[0]} beats ${choices[2]}\n--You lose!--`);
 	} else {
 		for (let i = 0; i < choices.length; i++) {
 			if (player == choices[i]) {
-				console.log(`${player} vs ${computer}\nIt's a tie!`);
+				console.log(`${player} vs ${computer}\n--It's a tie!--`);
 			}
 		}
 	}
 
 	return point;
 }
+
+function playGame() {
+	let score = 0;
+
+	for (let i = 0; i < 5; i++) {
+		const player = getPlayerChoice();
+		const computer = getComputerChoice();
+		score += playRound(player, computer);
+	}
+
+	console.log("Score: ", score);
+}
+
+playGame();
